9bd626ba773a183c48f91a05238b9dbf3cbe14d8
kangyuri1114 <83583757+kangyuri1114@users.noreply.github.com>
2023-05-22 03:31:34 +0900

    Delete Login_Architecture_Pattern/Architecture_Pattern_Login directory

--- Login_Architecture_Pattern/Architecture_Pattern_Login/app/src/main/java/com/example/architecture_pattern_login/MVPLoginView.kt
+++
@@ -1,8 +0,0 @@
-package com.example.architecture_pattern_login
-
-interface MVPLoginView {
-    fun getUsername(): String
-    fun getPassword(): String
-    fun showSuccessMessage()
-    fun showErrorMessage()
-}
