c64da67c57bcb91d5b11cad1ae4dd28ca4d98f98
kangyuri1114 <yuri3334@gmail.com>
2023-05-22 03:30:48 +0900

    MVVM Pattern

---
+++ Login_Architecture_Pattern/Login/app/src/main/java/com/example/login/MVVMLoginViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.login
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class MVVMLoginViewModel : ViewModel() {
+    private val userModel = UserModel("", "")
+    val username: MutableLiveData<String> = MutableLiveData()
+    val password: MutableLiveData<String> = MutableLiveData()
+
+    fun onLoginClicked() {
+
+        val enteredUsername = username.value
+        val enteredPassword = password.value
+
+        userModel.username = enteredUsername!!
+        userModel.password = enteredPassword!!
+
+        val isAuthenticated = userModel.authenticate()
+
+        if (isAuthenticated) {
+        }
+    }
+}
